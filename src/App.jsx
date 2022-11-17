6ac841ed0bcf71df7386e9789df80ab322e9909b
J.R Perez <jrbaprz@gmail.com>
2022-11-17 14:24:03 -0500

    Added array state to display tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
 function App() {
-  const [item, setItem] = useState("")
   const [list, setList] = useState([])
+  const [item, setItem] = useState("")
 
   return (
     <>
@@ -14,13 +14,21 @@ function App() {
               placeholder="What would you like to do?" 
               className="w-2/3 py-2 px-4 mr-2 rounded shadow-xl font-light focus:outline-none focus:shadow-lg focus:shadow-slate-200 duration-100 shadow-gray-100 lg:w-1/3" 
               onChange={(e) => {
-                setItem(e.target.value)
-                console.log(item)
+                setItem(e.target.value)                
               }}
           />
           <button 
             className="px-4 rounded shadow-sm font-light outline-slate-300 bg-[#ECEBE7] hover:bg-[#E1DFDB] transition transition-250"
+            onClick={() => {
+              setList(list => list.concat(item));
+            }}  
           >+</button>
+      </div>
+
+      <div className="flex mx-20 mt-10">
+          <li className='flex flex-col'>
+            {list}
+          </li>
       </div>
     </>
   )
